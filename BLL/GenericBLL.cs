2218d21abbe1c69ef6769ca4e1eae421e1e1f235
luonghau1808 <luonghau1808182006@gmail.com>
2025-07-26 19:09:59 +0700

    Lớp DAL và BLL

--- BLL/GenericBLL.cs
+++ BLL/GenericBLL.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using DuAn1_Nhom4.DAL;
+using DuAn1_Nhom4.Data;
 
 namespace DuAn1_Nhom4.BLL
 {
-    internal class GenericBLL
+    public class GenericBLL<T> where T : class
     {
+        GenericDAL<T> dAL;
+        public GenericBLL()
+        {
+            var context = new QuanLyKhoHangQuanAoContext();
+            dAL = new GenericDAL<T>(context);
+        }
+        public IEnumerable<T> GetAll(params Expression<Func<T, object>>[] inclues) => dAL.GetAll(inclues);
+        public T? GetById(int id) => dAL.GetById(id);
+        public void Add(T entity) => dAL.Add(entity);
+        public void Update(T entity) => dAL.Update(entity);
+        public void Delete(int id) => dAL.Delete(id);
     }
 }
