74e8793b6826b02be3ae1dccd9437d59daf74f0a
Nguyen Minh Duc <bbyshwv@gmail.com>
2025-08-05 11:46:27 +0700

    Da giai quyet

---
+++ GUI/ThongKe.Designer.cs
@@ -0,0 +1,448 @@
+﻿namespace DuAn1_Nhom4.GUI
+{
+    partial class ThongKe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            button2 = new Button();
+            button1 = new Button();
+            comboBox3 = new ComboBox();
+            comboBox2 = new ComboBox();
+            comboBox1 = new ComboBox();
+            dateTimePicker2 = new DateTimePicker();
+            dateTimePicker1 = new DateTimePicker();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            groupBox2 = new GroupBox();
+            label6 = new Label();
+            groupBox3 = new GroupBox();
+            panel4 = new Panel();
+            label10 = new Label();
+            panel3 = new Panel();
+            label7 = new Label();
+            panel2 = new Panel();
+            label9 = new Label();
+            panel1 = new Panel();
+            label8 = new Label();
+            dataGridView1 = new DataGridView();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            label15 = new Label();
+            label16 = new Label();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            panel4.SuspendLayout();
+            panel3.SuspendLayout();
+            panel2.SuspendLayout();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(comboBox3);
+            groupBox1.Controls.Add(comboBox2);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(dateTimePicker2);
+            groupBox1.Controls.Add(dateTimePicker1);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(467, 299);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Bộ lọc thống kê";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(320, 253);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 14;
+            button2.Text = "Làm mới";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(39, 253);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 13;
+            button1.Text = "Thống kê";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(292, 205);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(151, 28);
+            comboBox3.TabIndex = 12;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(292, 158);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(151, 28);
+            comboBox2.TabIndex = 11;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(292, 116);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(151, 28);
+            comboBox1.TabIndex = 10;
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Location = new Point(193, 77);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(250, 27);
+            dateTimePicker2.TabIndex = 9;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(193, 38);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(250, 27);
+            dateTimePicker1.TabIndex = 8;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 10.2F);
+            label5.Location = new Point(15, 205);
+            label5.Name = "label5";
+            label5.Size = new Size(92, 23);
+            label5.TabIndex = 7;
+            label5.Text = "Nhân viên:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 10.2F);
+            label4.Location = new Point(15, 158);
+            label4.Name = "label4";
+            label4.Size = new Size(91, 23);
+            label4.TabIndex = 6;
+            label4.Text = "Sản phẩm:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10.2F);
+            label3.Location = new Point(15, 116);
+            label3.Name = "label3";
+            label3.Size = new Size(118, 23);
+            label3.TabIndex = 5;
+            label3.Text = "Loại thống kê:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 10.2F);
+            label1.Location = new Point(15, 77);
+            label1.Name = "label1";
+            label1.Size = new Size(87, 23);
+            label1.TabIndex = 4;
+            label1.Text = "Đến ngày:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10.2F);
+            label2.Location = new Point(15, 38);
+            label2.Name = "label2";
+            label2.Size = new Size(75, 23);
+            label2.TabIndex = 3;
+            label2.Text = "Từ ngày:";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(label6);
+            groupBox2.Location = new Point(498, 12);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(1132, 220);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 10.2F);
+            label6.Location = new Point(497, 23);
+            label6.Name = "label6";
+            label6.Size = new Size(154, 23);
+            label6.TabIndex = 7;
+            label6.Text = "Biểu đồ + tổng kết";
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(panel4);
+            groupBox3.Controls.Add(panel3);
+            groupBox3.Controls.Add(panel2);
+            groupBox3.Controls.Add(panel1);
+            groupBox3.Location = new Point(498, 238);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(1132, 146);
+            groupBox3.TabIndex = 2;
+            groupBox3.TabStop = false;
+            groupBox3.Enter += groupBox3_Enter;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.Cyan;
+            panel4.BorderStyle = BorderStyle.FixedSingle;
+            panel4.Controls.Add(label14);
+            panel4.Controls.Add(label10);
+            panel4.Location = new Point(881, 38);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(162, 94);
+            panel4.TabIndex = 4;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 10.2F);
+            label10.Location = new Point(16, 11);
+            label10.Name = "label10";
+            label10.Size = new Size(129, 23);
+            label10.TabIndex = 5;
+            label10.Text = "Tổng sản phẩm";
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.Lime;
+            panel3.BorderStyle = BorderStyle.FixedSingle;
+            panel3.Controls.Add(label13);
+            panel3.Controls.Add(label7);
+            panel3.Location = new Point(619, 38);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(162, 94);
+            panel3.TabIndex = 3;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 10.2F);
+            label7.Location = new Point(45, 11);
+            label7.Name = "label7";
+            label7.Size = new Size(72, 23);
+            label7.TabIndex = 5;
+            label7.Text = "Tồn kho";
+            label7.Click += label7_Click_1;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Yellow;
+            panel2.BorderStyle = BorderStyle.FixedSingle;
+            panel2.Controls.Add(label12);
+            panel2.Controls.Add(label9);
+            panel2.Location = new Point(346, 38);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(162, 94);
+            panel2.TabIndex = 2;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 10.2F);
+            label9.Location = new Point(12, 11);
+            label9.Name = "label9";
+            label9.Size = new Size(141, 23);
+            label9.TabIndex = 5;
+            label9.Text = "Tổng phiếu nhập";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(255, 128, 0);
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(label11);
+            panel1.Controls.Add(label8);
+            panel1.Location = new Point(78, 38);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(162, 94);
+            panel1.TabIndex = 1;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 10.2F);
+            label8.Location = new Point(12, 11);
+            label8.Name = "label8";
+            label8.Size = new Size(135, 23);
+            label8.TabIndex = 5;
+            label8.Text = "Tổng phiếu xuất";
+            label8.Click += label8_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 402);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(1618, 211);
+            dataGridView1.TabIndex = 3;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(48, 50);
+            label11.Name = "label11";
+            label11.Size = new Size(58, 20);
+            label11.TabIndex = 6;
+            label11.Text = "label11";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(57, 50);
+            label12.Name = "label12";
+            label12.Size = new Size(58, 20);
+            label12.TabIndex = 6;
+            label12.Text = "label12";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(45, 50);
+            label13.Name = "label13";
+            label13.Size = new Size(58, 20);
+            label13.TabIndex = 6;
+            label13.Text = "label13";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(52, 50);
+            label14.Name = "label14";
+            label14.Size = new Size(58, 20);
+            label14.TabIndex = 6;
+            label14.Text = "label14";
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Font = new Font("Segoe UI", 10.2F);
+            label15.Location = new Point(27, 347);
+            label15.Name = "label15";
+            label15.Size = new Size(203, 23);
+            label15.TabIndex = 8;
+            label15.Text = "Tổng doanh thu của kho:";
+            label15.Click += label15_Click;
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Location = new Point(332, 349);
+            label16.Name = "label16";
+            label16.Size = new Size(58, 20);
+            label16.TabIndex = 9;
+            label16.Text = "label16";
+            // 
+            // ThongKe
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1642, 625);
+            Controls.Add(label16);
+            Controls.Add(label15);
+            Controls.Add(dataGridView1);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "ThongKe";
+            Text = "ThongKe";
+            Load += ThongKe_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private ComboBox comboBox2;
+        private ComboBox comboBox1;
+        private DateTimePicker dateTimePicker2;
+        private DateTimePicker dateTimePicker1;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label1;
+        private Label label2;
+        private ComboBox comboBox3;
+        private Button button2;
+        private Button button1;
+        private Label label6;
+        private GroupBox groupBox3;
+        private Panel panel1;
+        private Label label8;
+        private Panel panel2;
+        private Label label9;
+        private Panel panel3;
+        private Label label7;
+        private Panel panel4;
+        private Label label10;
+        private DataGridView dataGridView1;
+        private Label label14;
+        private Label label13;
+        private Label label12;
+        private Label label11;
+        private Label label15;
+        private Label label16;
+    }
+}
---
+++ GUI/ThongKe.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace DuAn1_Nhom4.GUI
+{
+    public partial class ThongKe : Form
+    {
+        public ThongKe()
+        {
+            InitializeComponent();
+        }
+
+        private void ThongKe_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label15_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
