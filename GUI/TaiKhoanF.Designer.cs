d8ca8f91c3f4e9ff40569c68d69c44763db6a652
luonghau1808 <luonghau1808182006@gmail.com>
2025-08-07 19:02:09 +0700

    no message

--- GUI/TaiKhoanF.Designer.cs
+++ GUI/TaiKhoanF.Designer.cs
@@ -43,7 +43,6 @@
             txtHoTen = new TextBox();
             txtMa = new TextBox();
             groupBox2 = new GroupBox();
-            picEyeConfirm = new PictureBox();
             picEyeNew = new PictureBox();
             label13 = new Label();
             label11 = new Label();
@@ -57,10 +56,11 @@
             label6 = new Label();
             txtMkCu = new TextBox();
             txtTenDangNhap = new TextBox();
+            picEyeConfirm = new PictureBox();
             groupBox1.SuspendLayout();
             groupBox2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)picEyeConfirm).BeginInit();
             ((System.ComponentModel.ISupportInitialize)picEyeNew).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)picEyeConfirm).BeginInit();
             SuspendLayout();
             // 
             // label1
@@ -231,23 +231,12 @@
             groupBox2.TabStop = false;
             groupBox2.Text = "Thông tin tài khoản";
             // 
-            // picEyeConfirm
-            // 
-            picEyeConfirm.Cursor = Cursors.Hand;
-            picEyeConfirm.Location = new Point(1011, 304);
-            picEyeConfirm.Name = "picEyeConfirm";
-            picEyeConfirm.Size = new Size(33, 31);
-            picEyeConfirm.SizeMode = PictureBoxSizeMode.Zoom;
-            picEyeConfirm.TabIndex = 24;
-            picEyeConfirm.TabStop = false;
-            picEyeConfirm.Click += picEyeConfirm_Click;
-            // 
             // picEyeNew
             // 
             picEyeNew.Cursor = Cursors.Hand;
-            picEyeNew.Location = new Point(1011, 227);
+            picEyeNew.Location = new Point(1011, 232);
             picEyeNew.Name = "picEyeNew";
-            picEyeNew.Size = new Size(33, 31);
+            picEyeNew.Size = new Size(33, 25);
             picEyeNew.SizeMode = PictureBoxSizeMode.Zoom;
             picEyeNew.TabIndex = 23;
             picEyeNew.TabStop = false;
@@ -373,6 +362,17 @@
             txtTenDangNhap.Size = new Size(457, 31);
             txtTenDangNhap.TabIndex = 11;
             // 
+            // picEyeConfirm
+            // 
+            picEyeConfirm.Cursor = Cursors.Hand;
+            picEyeConfirm.Location = new Point(1011, 310);
+            picEyeConfirm.Name = "picEyeConfirm";
+            picEyeConfirm.Size = new Size(33, 25);
+            picEyeConfirm.SizeMode = PictureBoxSizeMode.Zoom;
+            picEyeConfirm.TabIndex = 24;
+            picEyeConfirm.TabStop = false;
+            picEyeConfirm.Click += picEyeConfirm_Click;
+            // 
             // TaiKhoanF
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
@@ -387,8 +387,8 @@
             groupBox1.PerformLayout();
             groupBox2.ResumeLayout(false);
             groupBox2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)picEyeConfirm).EndInit();
             ((System.ComponentModel.ISupportInitialize)picEyeNew).EndInit();
+            ((System.ComponentModel.ISupportInitialize)picEyeConfirm).EndInit();
             ResumeLayout(false);
         }
 
